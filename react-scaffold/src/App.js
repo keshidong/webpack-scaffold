86aed4fde92874b16505b599aa8788f562b93eee
keshidong <shidongke213666@sohu-inc.com>
2017-12-24 18:38:36 +0800

    INIT

---
+++ react-scaffold/src/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+    render() {
+        return (
+            <div>hello china!
+            </div>
+        )
+    }
+}
+
+export default App;
